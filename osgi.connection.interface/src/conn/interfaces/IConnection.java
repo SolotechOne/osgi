5d0a97265ba2eb15be8ac4d7c45c8d809c2308f1
Carsten <Carsten@DESKTOP-6UQGA46.home>
2018-05-20 09:12:05 +0200

    initial commit

---
+++ osgi.connection.interface/src/conn/interfaces/IConnection.java
@@ -0,0 +1,10 @@
+package conn.interfaces;
+
+
+public interface IConnection {
+	public void open(String servername, int port);
+	
+	public void close();
+	
+	public void login(int client, String user, String department, String password, char language);
+}
