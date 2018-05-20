dbf030f7e49f9e53738de62e78edf5996c2e4f7c
Carsten <Carsten@DESKTOP-6UQGA46.home>
2018-05-20 10:45:27 +0200

    initial commit

---
+++ osgi.connection.implementation/src/osgi/connection/implementation/ConnectionDS.java
@@ -0,0 +1,51 @@
+package osgi.connection.implementation;
+
+import conn.interfaces.IConnection;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+
+@Component(
+		service=ConnectionDS.class,
+		name="ConnectionDS",
+		property = {
+				"description=declarative service connection",
+				"description=declarative service connection"
+			    }
+		)
+public class ConnectionDS implements IConnection{
+    @Activate
+    void activate() {
+        System.out.println("ConnectionDS activated");
+    }
+
+    @Modified
+    void modified() {
+        System.out.println("ConnectionDS modified");
+    }
+
+    @Deactivate
+        void deactivate() {
+        System.out.println("ConnectionDS deactivated");
+    }
+
+	@Override
+	public void open(String servername, int port) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void login(int client, String user, String department, String password, char language) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ osgi.connection.implementation/src/osgi/connection/implementation/Session.java
@@ -0,0 +1,67 @@
+package osgi.connection.implementation;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+import conn.interfaces.ISession;
+
+@Component(
+		service=Session.class,
+		name="Session"
+		)
+public class Session implements ISession {
+	private Connection connection;
+	
+	@Override
+	public String getSystemName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getWelcomeMessage() {
+		// TODO Auto-generated method stub
+		return "Welcome message";
+	}
+
+	@Override
+	public boolean isLoginSuccessful() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+    @Activate
+    void activate() {
+        System.out.println("Session activated");
+    }
+
+    @Modified
+    void modified() {
+        System.out.println("Session modified");
+    }
+
+    @Deactivate
+    void deactivate() {
+        System.out.println("Session deactivated");
+    }
+
+
+	@Reference
+	void setConnection(Connection connection) {
+		this.connection = connection;
+		System.out.println("Connection set");
+	}
+
+	void updateConnection(Connection connection) {
+		this.connection = connection;
+		System.out.println("Connection updated");
+	}
+
+	void unsetConnection(Connection connection) {
+		this.connection = null;
+		System.out.println("Connection unset");
+	}
+}
