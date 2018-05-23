e51e701deb652950b961963b0e6d1e1b82e11959
c.berberich <c.berberich@c1597nb1420.de.globusgrp.org>
2018-05-23 11:32:42 +0200

    indentation corrected

--- osgi.component.configuration/src/osgi/component/configuration/AdminConfiguredComponent.java
+++ osgi.component.configuration/src/osgi/component/configuration/AdminConfiguredComponent.java
@@ -29,7 +29,7 @@ public class AdminConfiguredComponent {
     }
 
     @Deactivate
-        void deactivate() {
+    void deactivate() {
         System.out.println("AdminConfiguredComponent deactivated");
         System.out.println();
     }
