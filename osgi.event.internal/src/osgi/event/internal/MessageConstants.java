95688f826ad75004adffad145614ebde606064fb
Carsten <Carsten@DESKTOP-6UQGA46.home>
2018-05-25 22:40:29 +0200

    new const for system

--- osgi.event.internal/src/osgi/event/internal/MessageConstants.java
+++ osgi.event.internal/src/osgi/event/internal/MessageConstants.java
@@ -1,8 +1,8 @@
 package osgi.event.internal;
 
-public final class Constants {
+public final class MessageConstants {
  
-    private Constants() {
+    private MessageConstants() {
         // private default constructor for constants class
         // to avoid someone extends the class
     }
@@ -11,6 +11,7 @@ public final class Constants {
     public static final String TOPIC_MESSAGE = TOPIC_BASE + "MESSAGE";
     public static final String TOPIC_ALL = TOPIC_BASE + "*";
  
+    public static final String PROPERTY_KEY_MESSAGE_SYSTEM = "system";
     public static final String PROPERTY_KEY_MESSAGE_TEXT = "text";
     public static final String PROPERTY_KEY_MESSAGE_TIMESTAMP = "timestamp";
     public static final String PROPERTY_KEY_MESSAGE_TYPE = "type";
