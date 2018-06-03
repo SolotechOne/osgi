bf061025cfba61de55491fa3125573f38de4e419
Carsten <Carsten@DESKTOP-6UQGA46.home>
2018-06-03 18:27:28 +0200

    initial commit

---
+++ osgi.file.install/src/osgi/file/install/Activator.java
@@ -0,0 +1,48 @@
+package osgi.file.install;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.ManagedService;
+
+public class Activator implements BundleActivator {
+
+	private ServiceRegistration<?> serviceRegistration;
+	
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		
+		ConfigurationService service = new ConfigurationService();
+		
+	    Dictionary<String,String> properties = new Hashtable<String,String>();
+	    properties.put(Constants.SERVICE_PID, "configurationservice" );
+	    
+	    String[] classes = new String[] { ConfigurationService.class.getName(), ManagedService.class.getName() };
+	    
+	    serviceRegistration = bundleContext.registerService( classes, service, properties );
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+		
+		serviceRegistration.unregister();
+	}
+}
---
+++ osgi.file.install/src/osgi/file/install/ConfigurationService.java
@@ -0,0 +1,15 @@
+package osgi.file.install;
+
+import java.util.Dictionary;
+
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+
+public class ConfigurationService implements ManagedService {
+	@Override
+	public void updated(Dictionary<String, ?> properties) throws ConfigurationException {
+		if( properties != null ) {
+			System.out.println( "port = " + properties.get( "port" ) );
+		}
+	}
+}
