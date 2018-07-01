8bb3c54f0080ddbd66855ae7199d3518361f211d
Carsten <Carsten@DESKTOP-6UQGA46.home>
2018-07-01 13:18:32 +0200

    initial commit

---
+++ osgi.equinox.app/src/osgi/equinox/app/HelloApp.java
@@ -0,0 +1,18 @@
+package osgi.equinox.app;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class HelloApp implements IApplication {
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		System.out.println("Hello from Eclipse application");
+		
+        return IApplication.EXIT_OK;
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+	}
+}
