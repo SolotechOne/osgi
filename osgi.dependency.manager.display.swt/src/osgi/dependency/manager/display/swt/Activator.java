0bae6f37ac7dfcbfd93110059ae2fb9d1d8c15c5
c.berberich <c.berberich@c1597nb1420.de.globusgrp.org>
2018-07-03 15:15:02 +0200

    initial commit

--- osgi.dependency.manager.display.swt/src/osgi/dependency/manager/display/swt/Activator.java
+++ osgi.dependency.manager.display.swt/src/osgi/dependency/manager/display/swt/Activator.java
@@ -1,19 +1,10 @@
 package osgi.dependency.manager.display.swt;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
+import javax.inject.Inject;
 
 import org.apache.felix.dm.DependencyActivatorBase;
 import org.apache.felix.dm.DependencyManager;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -22,12 +13,14 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
+
 import org.osgi.framework.BundleContext;
 
 import osgi.dependency.manager.temperature.interfaces.TemperatureService;
 
 public class Activator extends DependencyActivatorBase {
 
+//	@Inject
 	private volatile TemperatureService temperatureService;
 	
 	private Display display;
@@ -35,32 +28,30 @@ public class Activator extends DependencyActivatorBase {
 	private Button button;
 	private Text text;
 	
-//	private static BundleContext context;
-//
-//	static BundleContext getContext() {
-//		return context;
-//	}
-//
-//	/*
-//	 * (non-Javadoc)
-//	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-//	 */
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
 //	public void start(BundleContext bundleContext) throws Exception {
 //		Activator.context = bundleContext;
 //	}
-//
-//	/*
-//	 * (non-Javadoc)
-//	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-//	 */
-//	public void stop(BundleContext bundleContext) throws Exception {
-//		Activator.context = null;
-//	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
 
 	@Override
 	public void init(BundleContext context, DependencyManager manager) throws Exception {
-		createUI();
-
 		manager.add(createComponent()
 				.setImplementation(this)
 				.add(createServiceDependency()
@@ -68,6 +59,8 @@ public class Activator extends DependencyActivatorBase {
 						.setAutoConfig(true)     // Needed because with callbacks, auto config is not enabled by default
 						.setCallbacks("serviceAdded", "serviceRemoved"))
 				);
+
+		createUI();
 	}
 	
 	@Override
@@ -77,24 +70,28 @@ public class Activator extends DependencyActivatorBase {
 
     @SuppressWarnings("unused")
 	private void serviceAdded() {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-            	System.out.println("serviceAdded");
+    	System.out.println("serviceAdded");
+    	
+//        SwingUtilities.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//            	System.out.println("serviceAdded");
 //                enableUI(true);
 //                updateUI();
-            }
-        });
+//            }
+//        });
     }
 
     @SuppressWarnings("unused")
 	private void serviceRemoved() {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
+    	System.out.println("serviceRemoved");
+    	
+//        SwingUtilities.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
 //                enableUI(false);
-            }
-        });
+//            }
+//        });
     }
     
     private void updateUI() {
@@ -130,10 +127,10 @@ public class Activator extends DependencyActivatorBase {
 		
 		shell.open();
 
-//		while (!shell.isDisposed()) {
-//			if (!display.readAndDispatch()) {
-//				display.sleep();
-//			}
-//		}
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
     }
 }
